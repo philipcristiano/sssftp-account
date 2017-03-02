161c11aa8fd73d4456064d2f0e8655fddbdc8d19
Philip Cristiano <philipcristiano@philipcristiano.com>
2017-03-02 05:57:16 -0500

    Create bucket and role

--- main.tf
+++ main.tf
@@ -10,3 +10,27 @@ resource "aws_s3_bucket" "sssftp_account_test_bucket" {
         Name = "Account-Test Bucket"
     }
 }
+
+resource "aws_iam_role" "sssftp_role" {
+    name = "sssftp_role"
+    assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "AWS": "${var.sssftp_account_number}"
+      },
+      "Condition": {
+        "StringEquals": {
+          "sts:ExternalId": "${var.sssftp_external_id}"
+        }
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "sssftp_account_number" {}
+variable "sssftp_external_id" {}
